32299d05aeb0d1b606d3faa155a3bc24cd61ff23
bky373 <bo373@naver.com>
2019-12-07 18:38:49 +0900

    done #2-4 in making Calculator_RealNum

---
+++ calculator_realnum/src/main/java/com/example/calculator_realnum/RealNumCalculatorMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculator_realnum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RealNumCalculatorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_num_calculator_main);
+    }
+}
